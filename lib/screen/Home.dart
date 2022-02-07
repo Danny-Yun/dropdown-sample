33f4a7ea8d63208753de7693536674785682d50e
Danny-Yun <lacvert13@gmail.com>
2022-02-07 12:26:40 +0900

    주석 추가

--- lib/screen/Home.dart
+++ lib/screen/Home.dart
@@ -19,6 +19,7 @@ class Home extends StatelessWidget {
       ),
       body: Column(
         children: [
+          // DropdownButtonWidget(),
           PopupMenuButtonWidget(),
           Obx(() {
             switch (DropdownButtonController.to.currentItem.value) {
